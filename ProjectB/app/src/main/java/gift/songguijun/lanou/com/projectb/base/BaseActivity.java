deeb418e28c94d08b692a688938461d274102040
GuiJunEr <1106681822@qq.com>
2017-02-15 17:56:38 +0800

    base

---
+++ ProjectB/app/src/main/java/gift/songguijun/lanou/com/projectb/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package gift.songguijun.lanou.com.projectb.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 17/2/15.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
