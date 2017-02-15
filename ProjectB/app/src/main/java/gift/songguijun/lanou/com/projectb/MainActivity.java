f6ee2d09b7c4725c0b3771c5c85cd37657e3971f
GuiJunEr <1106681822@qq.com>
2017-02-15 17:53:16 +0800

    项目B

---
+++ ProjectB/app/src/main/java/gift/songguijun/lanou/com/projectb/MainActivity.java
@@ -0,0 +1,13 @@
+package gift.songguijun.lanou.com.projectb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
