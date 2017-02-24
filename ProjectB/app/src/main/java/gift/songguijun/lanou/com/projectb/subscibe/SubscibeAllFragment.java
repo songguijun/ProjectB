9e2a6ff28e1572d866973cb86cdab60505c2c549
Lin <291587302@qq.com>
2017-02-24 10:42:27 +0800

    订阅布局

---
+++ ProjectB/app/src/main/java/gift/songguijun/lanou/com/projectb/subscibe/SubscibeAllFragment.java
@@ -0,0 +1,26 @@
+package gift.songguijun.lanou.com.projectb.subscibe;
+
+import android.view.View;
+
+import gift.songguijun.lanou.com.projectb.base.BaseFragment;
+
+/**
+ * Created by dllo on 17/2/24.
+ */
+
+public class SubscibeAllFragment extends BaseFragment{
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(View itemView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- ProjectB/app/src/main/java/gift/songguijun/lanou/com/projectb/subscibe/SubscibeFragment.java
+++ ProjectB/app/src/main/java/gift/songguijun/lanou/com/projectb/subscibe/SubscibeFragment.java
@@ -2,22 +2,36 @@ package gift.songguijun.lanou.com.projectb.subscibe;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import gift.songguijun.lanou.com.projectb.R;
+import gift.songguijun.lanou.com.projectb.base.BaseFragment;
 
 /**
  * Created by dllo on 17/2/20.
  */
 
-public class SubscibeFragment extends Fragment {
-    @Nullable
+public class SubscibeFragment extends BaseFragment {
+    private TabLayout tabSubscibe ;
+    private ViewPager vpSubscibe ;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_subscibe,container,false);
-        return view;
+    protected int getLayoutId() {
+        return R.layout.fragment_subscibe;
+    }
+
+    @Override
+    protected void initView(View itemView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
